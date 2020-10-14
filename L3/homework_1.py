ff54a692326900cbda647b82da18b41c7e58e15c
Koneko <61724224+Koneko-TJ@users.noreply.github.com>
2020-10-14 16:40:16 +0900

    upload the answer files (.py)

---
+++ L3/homework_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 15:49:38 2020
+
+@author: George
+"""
+
+r = float(input("Please input r:"))
+result = 2 * r * 3.14
+print("The result is",  result)
+
---
+++ L3/homework_3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 16:01:50 2020
+
+@author: George
+"""
+
+h = float(input("Please input height:"))
+w = float(input("Please input width:"))
+d = float(input("Please input depth:"))
+
+result = h * w * d
+
+print("The result is",  result)
+
