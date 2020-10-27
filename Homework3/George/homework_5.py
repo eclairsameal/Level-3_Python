3773b0c34eed991891dbcc097e0186acf215a457
Fenrir <fenrireclair@gmail.com>
2020-10-27 15:23:37 +0900

    整理路徑

---
+++ Homework3/George/homework_5.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 16:08:05 2020
+
+@author: George
+"""
+
+a = float(input("Please input a:"))
+b = float(input("Please input b:"))
+c = float(input("Please input c:"))
+
+s = ( a + b + c ) / 2
+
+areaS = ( s * ( s - a ) * ( s - b ) * ( s - c )) ** 0.5
+
+print("The s is",  s)
+print("The S(Area) is",  areaS)
