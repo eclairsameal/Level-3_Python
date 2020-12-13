4235a38bb12c0eabb7039eea8d10ecf6ccd383c4
Fenrir <fenrireclair@gmail.com>
2020-12-13 19:28:33 +0900

    11_2 11_3 ans ok

--- Homework11/Fenrir/Homework11_2.py
+++ Homework11/Fenrir/Homework11_2.py
@@ -5,8 +5,7 @@ Created on Tue Dec  8 19:49:04 2020
 @author: Fenrir
 
 Description :
-    ユーザーに数値（自然数）を入力させる。
-    数字の１から入力された数値までの間で３で割り切れない数値を全て出力し表示させること
+
 Variable:
    input: n 
 """
---
+++ Homework11/Fenrir/Homework11_3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 19:49:04 2020
+
+@author: Fenrir
+
+Description :
+    ユーザーに自然数を1つ入力させ、その自然数の順番を反転させて出力する事。 
+    並びに入力された数値が0かどうかを検出し、そうであれば0を出力する事。
+Variable:
+   input: n 
+"""
+n = int(input("Please input number: "))
+
+while n != 0:
+    print(n % 10, end="")
+    n =int(n / 10)
+
+'''    
+n=input()
+print(n[::-1])
+'''  
