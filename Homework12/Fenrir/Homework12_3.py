d40f3eca6d25c39a3f87d2478efcc230f0b3dd3a
Fenrir <fenrireclair@gmail.com>
2020-12-21 19:52:32 +0900

    換檔名

--- Homework12/Fenrir/Homework12_3.py
+++ Homework12/Fenrir/Homework12_3.py
@@ -1,26 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 21 16:44:51 2020
+Created on Mon Dec 21 16:10:58 2020
 
 @author: Fenrir
 
 Description :
-    
+    乱数を8つ生成しそれをリストに格納し、そのリストに対し新たに数値を作成、挿入、削除等が可能になものを作ること。
 """
 import random
+
 list_number = []
-for i in range(8):
-    n = random.randint(1, 100)
-    list_number.append(n)
+for i in range(8): # 乱数を 8 つ生成
+    list_number.append(random.randint(1, 100))
 print(list_number)
 
-input_number = int(input("Input :"))
+while True:
+    print("1.Add(追加) 2.Insert(挿入) 3.Delete(削除) 4.End")
+    select = int(input("Please select function(機能を選択してください): "))
 
-flag = False
-for i in range(8):
-    if list_number[i] == input_number:
-        flag = True
-if flag:
-    print("The number is in the list")
-else:
-    print("Number is not in the list")
+    if select == 1:
+        n = int(input("New number（新しい数字）: "))
+        list_number.append(n)
+        # list_number.append(int(input("New number（新しい数字）: ")))
+        print(list_number)
+        
+    elif select == 2:
+        n = int(input("New number（新しい数字）:"))
+        p = int(input("Insertion position (the first position is 1)[挿入位置（最初の位置は 1）]: "))
+        list_number.insert(p - 1, n)
+        # list_number.insert(int(input("Insertion position (the first position is 1)[挿入位置（最初の位置は1）]:")) - 1, int(input("New number（新しい数字）:")))
+        print(list_number)
+        
+    elif select == 3:      
+        n = int(input("Deleted number（削除された数字）:"))
+        list_number.remove(n) 
+        # list_number.remove(int(input("Deleted number（削除された数字）:")))
+        print(list_number)
+        
+    elif select == 4: 
+        print("End")
+        break
+    
+    else:  # 1〜4以外で入力する場合
+        print("No option")
