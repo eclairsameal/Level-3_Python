d3a8d0b479325a307dac008ff62fec802b239e60
Fenrir <fenrireclair@gmail.com>
2021-01-25 20:18:33 +0900

    修改15的CODE

--- homework15/homework15_1.py
+++ homework15/homework15_1.py
@@ -6,7 +6,6 @@ Created on Mon Jan 25 14:52:18 2021
 
 Description :
 連続で入力できるようにする
-数値以外の物が入力された場合、それをリストには入れず再度入力させる（例えば、「abcde」と入力されたらリストには入れず再度入力を続行できるようにすること）
 「-9999」が入力されたら入力を終了し、全てを出力する（-9999はリストに含めず）
 最大値、最小値、合計値、平均値を出力する
 
@@ -17,10 +16,6 @@ while True:
     n = input("Input :")
     if n == "-9999":
         break
-    while not n.isdigit():
-        print("not number")
-        n = input("Input :")
-
     list_a.append(int(n))
   
 print("最大値:{}".format(max(list_a)))
--- homework15/homework15_2.py
+++ homework15/homework15_2.py
@@ -8,14 +8,38 @@ Description :
 
 
 """
+# list and in
+input_line = input()
+list_char = ['A', 'E', 'G', 'I', 'O', 'S', 'Z']
+list_number = [4, 3, 6, 1, 0, 5, 2]
 
-
-
-
-
-
-      
+for c in input_line:
+    if c in list_char:
+        print(list_number[list_char.index(c)], end = "") 
+    else: 
+        print(c, end = "")   
 """
+# if else
+input_line = input()
+for c in input_line:
+    if c == 'A':
+        print('4', end = "") 
+    elif c == 'E':
+        print("3", end = "") 
+    elif c == 'G':
+        print("6", end = "") 
+    elif c == 'I':
+        print("1", end = "") 
+    elif c == 'O':
+        print("0", end = "") 
+    elif c == 'S':
+        print("5", end = "") 
+    elif c == 'Z':
+        print("2", end = "") 
+    else:
+        print(c, end = "")   
+
+# function and map
 def Leet_fun(c):
     list_char = ['A', 'E', 'G', 'I', 'O', 'S', 'Z']
     list_number = [4, 3, 6, 1, 0, 5, 2]
