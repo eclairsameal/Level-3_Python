86b0ea90a72e55d8a29cb94f789f6a4b8f73b0d5
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-02-11 18:19:39 +0900

    Update homework17_2.py

--- homework17/homework17_2.py
+++ homework17/homework17_2.py
@@ -16,14 +16,26 @@ def compute(a, b): # * ユークリッドの互除法
     return b
 
 def compute_1(a, b): 
-    if a < b:  # 範囲として最小値を取ります
+    # 範囲として最小値を取ります
+    '''
+    if a < b:  
         min_val = a
     else:
         min_val = b
+    '''
+    min_val = (a, b)
     gcd_val = 1    
     for i in range(1, min_val + 1):
         if a % i == 0 and b % i == 0: # a と b の両方が割り切れる場合
             gcd_val = i        
+    return gcd_val
+
+def compute_2(a, b): 
+    min_val = (a, b) # 範囲として最小値を取ります
+    for i in range(min_val, 3, -1):
+        if a % i == 0 and b % i == 0: # a と b の両方が割り切れる場合
+            gcd_val = i  
+            break
     return gcd_val
 
 x = int(input("input x: "))
@@ -36,3 +48,4 @@ q = y * n
 
 print("{}/{} + {}/{} = {}/{}".format(x, y, m, n, int(p/compute(p, q)), int(q/compute(p, q))))
 print("{}/{} + {}/{} = {}/{}".format(x, y, m, n, int(p/compute_1(p, q)), int(q/compute_1(p, q))))
+print("{}/{} + {}/{} = {}/{}".format(x, y, m, n, int(p/compute_2(p, q)), int(q/compute_2(p, q))))
