164f1197bae81574ade39162261aa3f41ad7eaa2
Fenrir <fenrireclair@gmail.com>
2021-02-23 20:47:10 +0900

    HW21的解答

---
+++ homework21/homework21_1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 15:45:19 2021
+
+@author: Fenrir
+
+Description :
+
+"""
+string = input("Input:")
+re_str = list(string.replace(" ", "").lower()) # "aBCd" = "abcd"
+
+set_list = [] # 文字を格納する変数
+set_list_count = []# 文字数を格納する変数
+
+for x in re_str:
+    if not x in set_list: # この文字はリストにありますか？
+        set_list.append(x)
+        set_list_count.append(re_str.count(x)) # LISTにあるXの数を計算します
+
+for i in range(len(set_list)):
+    print("{}/{}: {}".format(set_list[i].upper(),set_list[i] ,set_list_count[i]))
---
+++ homework21/question.md
@@ -0,0 +1,13 @@
+## 1. 
+
+
+[Code example - F](https://github.com/eclairsameal/Level-3_Python/blob/main/homework21/homework21_1.py)
+
+
+
+## 2. 
+
+
+[Code example - F](https://github.com/eclairsameal/Level-3_Python/blob/main/homework21/homework21_2.py)
+
+
