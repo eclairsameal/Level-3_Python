14b091eccee76f84da873af1d71c29c360f4013e
Fenrir <fenrireclair@gmail.com>
2021-03-11 19:04:35 +0900

    檔名變更

--- homework21/homework21_1.py
+++ homework21/homework21_1.py
@@ -1,22 +1,52 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 22 15:45:19 2021
+Created on Mon Feb 22 16:11:30 2021
 
 @author: Fenrir
 
 Description :
 
 """
-string = input("Input:")
-re_str = list(string.replace(" ", "").lower()) # "aBCd" = "abcd"
 
-set_list = [] # 文字を格納する変数
-set_list_count = []# 文字数を格納する変数
+# not function
+string = "The Zen of Python, by Tim Peters\n\
+Beautiful is better than ugly.\n\
+Explicit is better than implicit.\n\
+Simple is better than complex.\n\
+Complex is better than complicated.\n\
+Flat is better than nested.\n\
+Sparse is better than dense.\n\
+Readability counts.\n\
+Special cases aren't special enough to break the rules.\n\
+Although practicality beats purity.\n\
+Errors should never pass silently.\n\
+Unless explicitly silenced.\n\
+In the face of ambiguity, refuse the temptation to guess.\n\
+There should be one-- and preferably only one --obvious way to do it.\n\
+Although that way may not be obvious at first unless you're Dutch.\n\
+Now is better than never.\n\
+Although never is often better than *right* now.\n\
+If the implementation is hard to explain, it's a bad idea.\n\
+If the implementation is easy to explain, it may be a good idea.\n\
+Namespaces are one honking great idea -- let's do more of those!"
 
-for x in re_str:
-    if not x in set_list: # この文字はリストにありますか？
-        set_list.append(x)
-        set_list_count.append(re_str.count(x)) # LISTにあるXの数を計算します
+before_encryption_list = [] # 暗号化前
+after_encryption_list = [] # 暗号化後
+before_encryption_count_list = [] 
+# print(string)
 
-for i in range(len(set_list)):
-    print("{}/{}: {}".format(set_list[i].upper(),set_list[i] ,set_list_count[i]))
+n = int(input("Please enter a few words you want to encrypt(暗号化したい単語をいくつか入力してください): "))
+for i in range(n):
+    before_encryption_list.append(input("Before encryption(暗号化前): "))
+    after_encryption_list.append(input("After encryption(暗号化後): "))    
+    before_encryption_count_list.append(string.count(before_encryption_list[i]))
+
+print("-" * 60)
+print("{:<25}{:<25}{:<10}".format("Before encryption", "After encryption", "Counts"))
+for i in range(n): 
+    print("{:<25}{:<25}{:<10}".format(before_encryption_list[i], after_encryption_list[i], before_encryption_count_list[i]))
+print("-" * 60)
+
+for i in range(n): 
+    string = string.replace(before_encryption_list[i], after_encryption_list[i])
+print(string)
